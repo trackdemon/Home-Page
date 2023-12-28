b285064976fafd2c94a0afd274a6ffbe8acd3daa
sharansudheer <saran_sudheer@hotmail.com>
2023-12-28 23:22:18 +0530

    Home Page Dump
    Styles

--- app/pages/home.js
+++ app/pages/home.js
@@ -1,2 +1,1122 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+/* Div 2 */
+
+position: absolute;
+width: 1440px;
+height: 825px;
+left: 0px;
+top: 100px;
+
+
+
+/* Div_2_Body */
+
+position: absolute;
+width: 1440px;
+height: 825px;
+left: 0px;
+top: 100px;
+
+/* Black Pearl */
+background: #1A1E21;
+
+
+/* Row 2 */
+
+position: absolute;
+width: 1250px;
+height: 500px;
+left: 100px;
+top: 400px;
+
+
+
+/* Card 5 - Travel */
+
+position: absolute;
+width: 450px;
+height: 500px;
+left: 900px;
+top: 400px;
+
+
+
+/* Card-5 */
+
+position: absolute;
+height: 500px;
+left: 900px;
+right: 90px;
+top: 400px;
+
+/* Tuna */
+background: #495057;
+mix-blend-mode: normal;
+box-shadow: 5px 5px 5px #010B13;
+border-radius: 20px;
+
+
+/* Group 229 */
+
+position: absolute;
+width: 236px;
+height: 440px;
+left: 1007px;
+top: 430px;
+
+
+
+/* TRAVELOGUES */
+
+position: absolute;
+height: 30px;
+left: 1007px;
+right: 197px;
+top: 430px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 30px;
+line-height: 30px;
+/* identical to box height, or 100% */
+letter-spacing: 0.1em;
+
+/* Light Blue grey */
+color: #E9ECEF;
+
+
+
+/* IMG_20211225_125932 */
+
+position: absolute;
+height: 350px;
+left: 1018px;
+right: 222px;
+top: 484px;
+
+background: url(IMG_20211225_125932.jpg);
+filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+
+/* Group 198 */
+
+position: absolute;
+width: 124px;
+height: 16px;
+left: 1051px;
+top: 854px;
+
+
+
+/* VISIT PAGE */
+
+position: absolute;
+height: 15px;
+left: 1051px;
+right: 311px;
+top: 855px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 15px;
+line-height: 15px;
+/* identical to box height, or 100% */
+
+color: #FFFFFF;
+
+
+
+/* box-arrow-up-right */
+
+position: absolute;
+height: 16px;
+left: 1159px;
+right: 265px;
+top: 854px;
+
+
+
+/* Group 195 */
+
+position: absolute;
+visibility: hidden;
+width: 13px;
+height: 13px;
+left: 1.5px;
+top: 1.5px;
+
+
+
+/* Rectangle (Stroke) */
+
+position: absolute;
+width: 11px;
+height: 11px;
+left: calc(50% - 11px/2 - 1px);
+top: calc(50% - 11px/2 + 12px);
+
+background: #000000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+width: 9px;
+height: 9px;
+left: calc(50% - 9px/2 + 2px);
+top: calc(50% - 9px/2 - 2px);
+
+background: #000000;
+
+
+/* box-arrow-bg-45 */
+
+position: absolute;
+left: 78.12%;
+right: -53.12%;
+top: 21.88%;
+bottom: 3.12%;
+
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-bg-45 */
+
+position: absolute;
+visibility: hidden;
+left: 100%;
+right: -100%;
+top: 0%;
+bottom: 0%;
+
+border: 1px solid #000000;
+border-radius: 1px;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-bg-45 (Stroke) */
+
+position: absolute;
+left: 104.17%;
+right: -112.5%;
+top: -4.17%;
+bottom: -4.17%;
+
+/* Cyan Blue */
+background: #CED4DA;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 */
+
+position: absolute;
+left: 96.88%;
+right: -53.12%;
+top: 3.12%;
+bottom: 40.62%;
+
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 */
+
+position: absolute;
+visibility: hidden;
+left: 100%;
+right: -100%;
+top: 0%;
+bottom: 0%;
+
+border: 1px solid #000000;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 (Stroke) */
+
+position: absolute;
+left: 105.56%;
+right: -116.67%;
+top: -5.56%;
+bottom: -5.56%;
+
+/* Cyan Blue */
+background: #CED4DA;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* Card 4 - Hi */
+
+position: absolute;
+width: 400px;
+height: 350px;
+left: 450px;
+top: 400px;
+
+
+
+/* Card-4 */
+
+position: absolute;
+height: 350px;
+left: 450px;
+right: 590px;
+top: 400px;
+
+/* Raven */
+background: #6C757D;
+box-shadow: 5px 5px 5px #010B13;
+border-radius: 20px;
+
+
+/* HI THERE 😁, */
+
+position: absolute;
+height: 36px;
+left: 475px;
+right: 730px;
+top: 429px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 36px;
+line-height: 36px;
+/* identical to box height, or 100% */
+letter-spacing: 0.1em;
+
+/* Cyan Blue */
+color: #CED4DA;
+
+
+
+/* trackdemon.in Is an Other side of Sharan’s world of Freelance Mobile and Back-End Web Application Development, primarily to showcase Proof of Concept works, Articles about issues that I have run into during Software Dev lifecycle and Travelogues */
+
+position: absolute;
+height: 198px;
+left: 475px;
+right: 615px;
+top: 490px;
+
+font-family: 'Seaweed Script';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 22px;
+
+color: #FFFFFF;
+
+
+
+/* box-arrow-up-right */
+
+position: absolute;
+height: 16px;
+left: 809px;
+right: 615px;
+top: 693px;
+
+
+
+/* Group 195 */
+
+position: absolute;
+visibility: hidden;
+width: 13px;
+height: 13px;
+left: 1.5px;
+top: 1.5px;
+
+
+
+/* Rectangle (Stroke) */
+
+position: absolute;
+width: 11px;
+height: 11px;
+left: calc(50% - 11px/2 - 1px);
+top: calc(50% - 11px/2 + 12px);
+
+background: #000000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+width: 9px;
+height: 9px;
+left: calc(50% - 9px/2 + 2px);
+top: calc(50% - 9px/2 - 2px);
+
+background: #000000;
+
+
+/* box-arrow-bg-45 */
+
+position: absolute;
+left: 78.12%;
+right: -53.12%;
+top: 21.88%;
+bottom: 3.12%;
+
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-bg-45 */
+
+position: absolute;
+visibility: hidden;
+left: 100%;
+right: -100%;
+top: 0%;
+bottom: 0%;
+
+border: 1px solid #000000;
+border-radius: 1px;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-bg-45 (Stroke) */
+
+position: absolute;
+left: 104.17%;
+right: -112.5%;
+top: -4.17%;
+bottom: -4.17%;
+
+/* Cyan Blue */
+background: #CED4DA;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 */
+
+position: absolute;
+left: 96.88%;
+right: -53.12%;
+top: 3.12%;
+bottom: 40.62%;
+
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 */
+
+position: absolute;
+visibility: hidden;
+left: 100%;
+right: -100%;
+top: 0%;
+bottom: 0%;
+
+border: 1px solid #000000;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 (Stroke) */
+
+position: absolute;
+left: 105.56%;
+right: -116.67%;
+top: -5.56%;
+bottom: -5.56%;
+
+/* Cyan Blue */
+background: #CED4DA;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* Card 3 - Open-Source */
+
+position: absolute;
+width: 300px;
+height: 350px;
+left: 100px;
+top: 400px;
+
+
+
+/* Card-3 */
+
+position: absolute;
+height: 350px;
+left: 100px;
+right: 1040px;
+top: 400px;
+
+/* Tuna */
+background: #495057;
+box-shadow: 5px 5px 5px #010B13;
+border-radius: 20px;
+
+
+/* Group 197 */
+
+position: absolute;
+width: 203px;
+height: 274px;
+left: 149px;
+top: 435px;
+
+
+
+/* OPEN SOURCE */
+
+position: absolute;
+height: 30px;
+left: 152px;
+right: 1091px;
+top: 435px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 25px;
+line-height: 30px;
+/* identical to box height, or 120% */
+letter-spacing: 0.1em;
+
+/* Cyan Blue */
+color: #CED4DA;
+
+
+
+/* Group 196 */
+
+position: absolute;
+width: 203px;
+height: 229px;
+left: 149px;
+top: 480px;
+
+
+
+/* On a side note, I am in the process of finding a solution to Integrate OpenEMR’s APIs using Spring Boot and Retrofit for the benefit of both Web and Mobile based developers. Hoping to contribute to other projects as well. */
+
+position: absolute;
+height: 229px;
+left: 149px;
+right: 1088px;
+top: 480px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 21px;
+
+color: #FFFFFF;
+
+
+
+/* box-arrow-up-right */
+
+position: absolute;
+height: 16px;
+left: 336px;
+right: 1088px;
+top: 693px;
+
+
+
+/* Group 195 */
+
+position: absolute;
+visibility: hidden;
+width: 13px;
+height: 13px;
+left: 1.5px;
+top: 1.5px;
+
+
+
+/* Rectangle (Stroke) */
+
+position: absolute;
+width: 11px;
+height: 11px;
+left: calc(50% - 11px/2 - 1px);
+top: calc(50% - 11px/2 + 12px);
+
+background: #000000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Vector */
+
+position: absolute;
+width: 9px;
+height: 9px;
+left: calc(50% - 9px/2 + 2px);
+top: calc(50% - 9px/2 - 2px);
+
+background: #000000;
+
+
+/* box-arrow-bg-45 */
+
+position: absolute;
+left: 78.12%;
+right: -53.12%;
+top: 21.88%;
+bottom: 3.12%;
+
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-bg-45 */
+
+position: absolute;
+visibility: hidden;
+left: 100%;
+right: -100%;
+top: 0%;
+bottom: 0%;
+
+border: 1px solid #000000;
+border-radius: 1px;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-bg-45 (Stroke) */
+
+position: absolute;
+left: 104.17%;
+right: -112.5%;
+top: -4.17%;
+bottom: -4.17%;
+
+/* Cyan Blue */
+background: #CED4DA;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 */
+
+position: absolute;
+left: 96.88%;
+right: -53.12%;
+top: 3.12%;
+bottom: 40.62%;
+
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 */
+
+position: absolute;
+visibility: hidden;
+left: 100%;
+right: -100%;
+top: 0%;
+bottom: 0%;
+
+border: 1px solid #000000;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* box-arrow-45 (Stroke) */
+
+position: absolute;
+left: 105.56%;
+right: -116.67%;
+top: -5.56%;
+bottom: -5.56%;
+
+/* Cyan Blue */
+background: #CED4DA;
+transform: matrix(-1, 0, 0, 1, 0, 0);
+
+
+/* Row 1 */
+
+position: absolute;
+width: 1250px;
+height: 200px;
+left: 95px;
+top: 150px;
+
+
+
+/* Card-2 TopIcons */
+
+position: absolute;
+width: 390px;
+height: 200px;
+left: 955px;
+top: 150px;
+
+
+
+/* Card-2 */
+
+position: absolute;
+height: 200px;
+left: 955px;
+right: 95px;
+top: 150px;
+
+/* Baltic Sea */
+background: #343A40;
+box-shadow: 5px 5px 5px #010B13;
+border-radius: 20px;
+
+
+/* Group 8 */
+
+position: absolute;
+width: 220px;
+height: 130px;
+left: 1040px;
+top: 185px;
+
+
+
+/* Group 6 */
+
+position: absolute;
+width: 220px;
+height: 40px;
+left: 1040px;
+top: 275px;
+
+
+
+/* github */
+
+position: absolute;
+height: 40px;
+left: 1130px;
+right: 270px;
+top: 275px;
+
+
+
+/* github */
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 2.47%;
+
+/* Cyan Blue */
+background: #CED4DA;
+
+
+/* envelope */
+
+position: absolute;
+height: 40px;
+left: 1220px;
+right: 180px;
+top: 275px;
+
+
+
+/* vector */
+
+position: absolute;
+visibility: hidden;
+width: 16px;
+height: 12px;
+left: 0px;
+top: 2px;
+
+
+
+/* Rectangle 144 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 16px;
+height: 12px;
+left: 0px;
+top: 2px;
+
+border: 1px solid #000000;
+border-radius: 2px;
+
+
+/* Mask Group */
+
+position: absolute;
+width: 16px;
+height: 12px;
+left: 0px;
+top: 2px;
+
+
+
+/* Rectangle 145 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 16px;
+height: 12px;
+left: 0px;
+top: 2px;
+
+background: #C4C4C4;
+border: 1px solid #000000;
+border-radius: 2px;
+
+
+/* Vector 129 */
+
+position: absolute;
+width: 18px;
+height: 4.73px;
+left: -1px;
+top: 8.27px;
+
+border: 1px solid #000000;
+
+
+/* Vector 130 */
+
+position: absolute;
+width: 18px;
+height: 5.5px;
+left: -1px;
+top: 3.5px;
+
+border: 1px solid #000000;
+
+
+/* Vector */
+
+position: absolute;
+visibility: hidden;
+width: 16px;
+height: 12px;
+left: 0px;
+top: 2px;
+
+background: #000000;
+
+
+/* Rectangle 256 (Stroke) */
+
+position: absolute;
+visibility: hidden;
+left: 0%;
+right: 0%;
+top: 12.5%;
+bottom: 12.5%;
+
+background: #000000;
+
+
+/* Rectangle 255 (Stroke) */
+
+position: absolute;
+visibility: hidden;
+width: 16px;
+height: 12px;
+left: 0px;
+top: 2px;
+
+background: #000000;
+
+
+/* v3 */
+
+position: absolute;
+visibility: hidden;
+width: 15px;
+height: 11px;
+left: 0.5px;
+top: 2.5px;
+
+border: 1px solid #000000;
+
+
+/* Rectangle 394 (Stroke) */
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 12.5%;
+bottom: 12.5%;
+
+/* Cyan Blue */
+background: #CED4DA;
+
+
+/* apple */
+
+position: absolute;
+height: 40px;
+left: 1040px;
+right: 360px;
+top: 275px;
+
+
+
+/* Vector */
+
+position: absolute;
+left: 9.38%;
+right: 9.37%;
+top: 0%;
+bottom: 0%;
+
+/* Cyan Blue */
+background: #CED4DA;
+
+
+/* Group 7 */
+
+position: absolute;
+width: 220px;
+height: 40px;
+left: 1040px;
+top: 185px;
+
+
+
+/* instagram */
+
+position: absolute;
+height: 40px;
+left: 1220px;
+right: 180px;
+top: 185px;
+
+
+
+/* instagram */
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 0%;
+
+/* Cyan Blue */
+background: #CED4DA;
+
+
+/* linkedin */
+
+position: absolute;
+height: 40px;
+left: 1130px;
+right: 270px;
+top: 185px;
+
+
+
+/* linkedin */
+
+position: absolute;
+left: 0%;
+right: 0%;
+top: 0%;
+bottom: 0%;
+
+/* Cyan Blue */
+background: #CED4DA;
+
+
+/* google-play */
+
+position: absolute;
+height: 40px;
+left: 1040px;
+right: 360px;
+top: 185px;
+
+
+
+/* Subtract */
+
+position: absolute;
+visibility: hidden;
+width: 36px;
+height: 41.11px;
+left: 5px;
+top: -0.55px;
+
+background: #000000;
+
+
+/* Polygon 14 */
+
+position: absolute;
+left: 106.25%;
+right: -131.25%;
+top: -12.5%;
+bottom: -12.5%;
+
+background: #D9D9D9;
+border-radius: 1.5px;
+transform: rotate(90deg);
+
+
+/* Union */
+
+position: absolute;
+width: 33.18px;
+height: 35.73px;
+left: -0.34px;
+top: 2.13px;
+
+background: #000000;
+
+
+/* Vector 480 */
+
+position: absolute;
+left: 0%;
+right: 18.75%;
+top: 18.75%;
+bottom: 6.25%;
+
+border: 1px solid #000000;
+
+
+/* Vector 481 */
+
+position: absolute;
+left: 0%;
+right: 18.75%;
+top: 81.25%;
+bottom: -56.25%;
+
+border: 1px solid #000000;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* Subtract */
+
+position: absolute;
+left: 6.25%;
+right: 6.25%;
+top: 0%;
+bottom: 0%;
+
+/* Cyan Blue */
+background: #CED4DA;
+
+
+/* Card 1-SDE */
+
+position: absolute;
+width: 800px;
+height: 200px;
+left: 95px;
+top: 150px;
+
+
+
+/* Card-1 */
+
+position: absolute;
+height: 200px;
+left: 95px;
+right: 545px;
+top: 150px;
+
+/* Blue Charcol */
+background: #212529;
+box-shadow: 5px 5px 5px #010B13;
+border-radius: 20px;
+
+
+/* SOFTWARE DEVELOPMENT ARTICLES */
+
+position: absolute;
+height: 20px;
+left: 336px;
+right: 705px;
+top: 174px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 20px;
+/* identical to box height, or 100% */
+letter-spacing: 0.1em;
+
+/* Cyan Blue */
+color: #CED4DA;
+
+mix-blend-mode: normal;
+
+
+/* Group 10 */
+
+position: absolute;
+width: 700px;
+height: 100px;
+left: 149px;
+top: 209px;
+
+
+
+/* Inner-Card */
+
+box-sizing: border-box;
+
+position: absolute;
+height: 100px;
+left: 149px;
+right: 591px;
+top: 209px;
+
+/* Raven */
+background: #6C757D;
+/* Cyan Blue */
+border: 1px solid #CED4DA;
+box-shadow: 5px 5px 5px rgba(1, 11, 19, 0.5);
+border-radius: 5px;
+
+
+/* MULTI DOMAIN - SINGLE SERVER - MICRO SERVICES APPROACH */
+
+position: absolute;
+height: 20px;
+left: 160px;
+right: 659px;
+top: 248px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 20px;
+/* identical to box height, or 100% */
+
+color: #FFFFFF;
+
+
+
+/* chevron-right */
+
+position: absolute;
+height: 16px;
+left: 815px;
+right: 609px;
+top: 249px;
+
+
+
+/* Path (Stroke) */
+
+position: absolute;
+left: 28.12%;
+right: 28.12%;
+top: 9.38%;
+bottom: 9.38%;
+
+/* White */
+background: #FFFFFF;
